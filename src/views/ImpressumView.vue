a895da2e40c08837825d55ac52037bb56d327394
MartinGoesToSauerland <98959955+MartinGoesToSauerland@users.noreply.github.com>
2022-11-07 21:13:05 +0100

    Add Impressum View

---
+++ src/views/ImpressumView.vue
@@ -0,0 +1,10 @@
+<template>
+    <main>
+        <section>
+            <div class="container">
+                <h1>Impressum</h1>
+                <div>anschrift</div>
+            </div>
+        </section>
+    </main>
+</template>
