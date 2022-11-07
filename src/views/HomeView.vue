fe576e0712bc04193c10afddf49435ea91b41b06
MartinGoesToSauerland <98959955+MartinGoesToSauerland@users.noreply.github.com>
2022-11-07 21:13:31 +0100

    Edit Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,42 +9,47 @@ import ContactForm from "../components/ContactForm.vue"
         <div class="parallax parallax-meadow">
           <div class="center-box text-center">
             <div>
-              <h1>hallo</h1>
-              <button class="btn btn-action" @click="$router.push({path: '/areas'})">Flächen buchen</button>
+              <h1>Werde ein Bienenheld!</h1>
+              <button class="btn btn-action" @click="$router.push({path: '/areas'})">Flächen auswählen</button>
             </div>
           </div>
-          
+
           <div class="w-1 ">
-            
+
             <div class="w-2">
               <div class="banner-a"></div>
-              <div class="banner-b"></div>  
+              <div class="banner-b"></div>
             </div>
           </div>
-          
+
         </div>
-        
+
       </section>
 
-      <section id="about">
+      <!--section id="about">
         <div class="s-orange">
 
-          <h2>1. Suche Dir eine geeignete Parzelle die du mieten möchtest</h2>
-          <h2>2. Wähle die Parzelle und miete sie für ein Jahr</h2>
-          <h2>3. ...und sicherer Bienen und anderen Insekten den Lebensraum.</h2>
+          <h2>Flächen verfügbar: 10.000qm/2</h2>
+          <h2>Flächen in Blühwiesen umgewandelt: 200qm/2</h2>
+          <h2>...</h2>
         </div>
-      </section>
+      </section-->
 
       <section id="work">
-        <div class="parallax parallax-hex">
-          WORK
+        <div class="parallax x-parallax-hex">
+          <div class="container text-center" style="column-count: 1; font-size: 1.1rem; color: black; padding:20px 10px 20px 10px;">
+            <h2>Kurzvorstellung</h2>
+            In den letzten Jahren ist ein zunehmendes Bienensterben zu beobachten. Die Ursachen hierfür sind vielfältig und reichen von Krankheiten über den Klimawandel bis hin zu bestimmten Formen der landwirtschaftlichen Nutzung. Die Folgen des zunehmenden Bienensterbens können zu einer ernsten Bedrohung werden. Wenn Bienen fehlen, stehen diese nicht mehr als Bestäuber der Pflanzen zur Verfügung. Etwa ein Drittel der menschlichen Lebensmittel aber wächst ausschließlich nach der Bestäubung durch Bienen. Folglich droht eine große Lücke in der menschlichen Versorgung, wenn die entsprechenden Nahrungsmittel wegfallen. Daher müssen alle Anstrengungen unternommen werden, um das zunehmende Bienensterben wieder zu stoppen.
+
+            Hier setzt die Idee von Bienchenoase an. Wir wollen bislang landwirtschaftlich genutzte Flächen komplett aus der landwirtschaftlichen Produktion herausnehmen, um dort bienenfreundliche Biotope zu schaffen. Dies erfordert insbesondere die Ansiedelung von Pflanzenarten, deren Blüten viel Pollen und Nektar produzieren. Die geschaffenen Biotope sollen den Bienen dann eine ausreichende Nahrungsgrundlage vom Frühjahr bis in den Herbst hinein bieten. Bienchenoase möchte daher Menschen vernetzen, welche einen Beitrag zum Schutz der Bienen leisten wollen. In diesem Rahmen werden wir landwirtschaftliche Flächen anpachten. Im Fokus stehen hierbei konventionell genutzte Flächen – also landwirtschaftliche Flächen mit einem intensiven Einsatz mineralischer Dünger sowie chemischer Pflanzenschutzmittel (Insektizide, Herbizide, Fungizide). Die angepachteten Flächen werden wir parzellieren und im Rahmen mit Ihnen geschlossener Patenschaften in bienenfreundliche Biotope umwandeln.
+          </div>
         </div>
       </section>
 
       <section id="contact">
           <ContactForm />
       </section>
-      
+
     </main>
 </template>
 
@@ -71,7 +76,7 @@ import ContactForm from "../components/ContactForm.vue"
     width: 300px;
     height:70px;
     background-repeat: no-repeat;
-    background-size: cover;    
+    background-size: cover;
     background-position: center center;
     border-radius: 8px;
     margin-bottom: 10px;
@@ -85,24 +90,4 @@ import ContactForm from "../components/ContactForm.vue"
     background-image: url('./images/logo-2.webp');
   }
 
-  .btn {
-    cursor: pointer;
-    padding: 10px 40px;
-    border-radius: var(--border-radius);
-    max-width:300px;
-  }
-  .btn:hover {
-    background: black;
-    color: white;
-    border-color: #999;
-    box-shadow: var(--shadow);
-  }
-  .btn-action {
-    border: 1px solid var(--white);
-    background-color: orange;
-    font-weight: bold;
-    box-shadow: var(--shadow);
-    color: white;
-    font-size: 1.5rem;
-  }
 </style>
